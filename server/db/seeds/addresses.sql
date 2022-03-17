e1a50412038e208b1112178c3c11684485c21eeb
bkcatton <ben.k.catton@gmail.com>
2022-03-17 14:32:59 -0700

    Add seed files

---
+++ server/db/seeds/addresses.sql
@@ -0,0 +1,38 @@
+INSERT INTO addresses (
+  id,
+  street_name,
+  street_number,
+  city,
+  state_code,
+  country,
+  postal_code,
+  user_id
+)
+VALUES
+(1, 
+'Elizabeth St', 
+'37a', 
+'New York', 
+'NY', 
+'10013', 
+'USA',
+1 
+),
+(2, 
+'Wooster St', 
+'59', 
+'New York', 
+'NY', 
+'10012', 
+'USA',
+2 
+),
+(3, 
+'Bayard St', 
+'87', 
+'New York', 
+'NY', 
+'10013', 
+'USA',
+3 
+),
---
+++ server/db/seeds/dishes.sql
@@ -0,0 +1,4 @@
+INSERT INTO dishes (id, title, dish_description, dish_type, price_cents, serving_size, image_link, country_style, available_stock)
+
+VALUES
+(1, "Chinese Sticky Rice Dumpling", "A traditional Chinese rish dish made of glutinous rice.", ""),
