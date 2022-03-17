2443eb51407d8624224fdc590e54906b66cc42e7
Albert Ho <albertcyi.ho@gmail.com>
2022-03-17 15:53:26 -0700

    feat - add boilerplate for routes

--- server/index.js
+++ server/index.js
@@ -1,14 +1,47 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const cors = require("cors");
-const PORT = 3000
+const cors = require('cors');
+const PORT = 3000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-// routes
+// PG database client/connection setup
+const { Pool } = require('pg');
+const dbParams = require('./lib/db.js');
+const db = new Pool(dbParams);
+db.connect();
 
+// user routes
+// 1. users can log in
+app.get('/login/:id', (req, res) => {
+  const { id } = req.params;
+
+  // set cookie = id
+  // redirect to "/"
+});
+
+// 1. users can log out
+app.get('/logout', (req, res) => {
+  // clear cookie
+  // redirect to "/"
+});
+
+// 3. users can browse all dishes and filter by culture, location, price, type [Google Maps API]
+app.get('/browse', async (req, res) => {
+  try {
+    const dish = await db.query('SELECT * FROM dishes WHERE id = 1');
+    res.json(dish);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+// api routes
+// users can send a message over text using twilio
+// app.use('/api/sendText', sendTextQuery(db));
+// app.use('/api/receiveText', sendTextQuery(db));
 
 // set-up
 app.listen(PORT, () => {
