c91fcdb47296877ed180b5a463aabe660fe2afe8
bkcatton <ben.k.catton@gmail.com>
2022-03-17 20:31:08 -0700

    feat - access api through react component

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 function App() {
+  const [addresses, setAddresses] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/addresses')
+      .then((data) => {
+        setAddresses(data.data.addresses)
+      })
+      .catch((e) => {
+        console.log(e)
+      })
+  }, [])
+
+  console.log(addresses)
+  const addressList = addresses.map(items => {
+    return <li>{items.street_name}</li>   
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>HI</h1>
+      <ul>{addressList}</ul>
     </div>
   );
 }
