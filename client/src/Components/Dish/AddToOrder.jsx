4f850cfb66358cbbe998fb42c2d5922a43135442
bkcatton <ben.k.catton@gmail.com>
2022-03-19 16:34:32 -0700

    feat - link to add to order page

--- client/src/Components/Dish/AddToOrder.jsx
+++ client/src/Components/Dish/AddToOrder.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 import axiosConfig from '../../axiosConfig';
 
 const Counter = props => {
  const [quantity, setQuantity] = useState(1);
 
+ // create an order, use newly created order_id to add order with details to cart
   const onSubmit = () => {
     axiosConfig.post('/orders').then(data => {
       const { id: order_id } = data.data;
@@ -11,18 +13,18 @@ const Counter = props => {
       const paid_price_cents = quantity * Number(props.dishDetails.price_cents);
 
       axiosConfig.post(`/orders/order_item`, { order_id, dish_id, quantity, paid_price_cents })
-      .then(data => console.log(data))
-      .catch(error => console.log(error))
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
     })
-    
-
   }
 
   return (
     <div>Quantity = {quantity}
       <button onClick={() => setQuantity(prev => prev + 1)}>+</button>
       <button onClick={() => setQuantity(prev => prev - 1)}>-</button>
-      <button onClick={onSubmit}>  Add to Order  </button>
+      <button onClick={onSubmit}>Add to Order</button>
+      <Link to="/orders"> Continue to Checkout </Link>
+      <Link to="/"> Continue Shopping </Link>
     </div>
   )
   
