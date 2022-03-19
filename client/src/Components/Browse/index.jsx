772eac054e3a4c1c2032ee097e7e9baa8d55566a
Albert Ho <albertcyi.ho@gmail.com>
2022-03-18 17:48:42 -0700

    feat - fetch dish info and reviews formatted

--- client/src/Components/Browse/index.jsx
+++ client/src/Components/Browse/index.jsx
@@ -1,18 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import MapContainer from './MapContainer';
 import VendorsList from './VendorsList';
 
 const Browse = () => {
+  const [info, setInfo] = useState([]);
+  const [allReviews, setAllReviews] = useState([]);
+
   useEffect(() => {
     Promise.all([
-      axios.get('http://localhost:8080/api/browse'),
-      axios.get('http://localhost:8080/api/browse'),
+      axios.get('http://localhost:8080/api/dishes/information'),
+      axios.get('http://localhost:8080/api/dishes/reviews'),
     ]).then(all => {
-      console.log(all[0], all[1]);
+      // console.log(all[0], all[1]);
+      setInfo(all[0].data);
+      setAllReviews(all[1].data);
     });
-
   }, []);
+
+  const infoReviews = info.map(item => {
+    item.reviews = [];
+
+    for (const review of allReviews) {
+      if (review.dish_id === item.id) {
+        item.reviews.push(review);
+      }
+    }
+
+    return item;
+  });
+  console.log(infoReviews);
 
   return (
     <div>
