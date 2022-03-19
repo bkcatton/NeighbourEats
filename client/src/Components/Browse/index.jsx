cffa86e9394c82456fae79aa06591330d3bb576e
bkcatton <ben.k.catton@gmail.com>
2022-03-19 14:31:53 -0700

    refactor - use async await for all axios calls

--- client/src/Components/Browse/index.jsx
+++ client/src/Components/Browse/index.jsx
@@ -19,7 +19,7 @@ const Browse = () => {
         axiosConfig.get('/dishes/reviews'),
         axiosConfig.get('/dishes/ratings')
       ]);
-      
+
       setDishesInfo(all[0].data);
       setDishesReviews(all[1].data);
       setDishesRatings(all[2].data);
