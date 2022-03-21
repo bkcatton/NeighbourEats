623fc9f99590e71311894013e272359032295931
Christian Girling <girling.christian@gmail.com>
2022-03-21 14:36:15 -0700

    feat - set up material ui packages

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,11 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import './index.css';
 import App from './App';
 import UserProvider from './Components/UserProvider';
