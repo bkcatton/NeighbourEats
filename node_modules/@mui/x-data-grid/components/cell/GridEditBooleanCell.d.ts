aea2de1c1d5d6881861da447e9f1fa0502617108
bkcatton <ben.k.catton@gmail.com>
2022-03-22 10:29:58 -0700

    feat - add datagrid to browse

--- node_modules/@mui/x-data-grid/components/cell/GridEditBooleanCell.d.ts
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-import { GridRenderEditCellParams } from '../../models/params/gridCellParams';
-import { GridColDef } from '../../models/colDef/gridColDef';
-export declare function GridEditBooleanCell(props: GridRenderEditCellParams & Omit<React.DetailedHTMLProps<React.LabelHTMLAttributes<HTMLLabelElement>, HTMLLabelElement>, 'id'>): JSX.Element;
-export declare const renderEditBooleanCell: GridColDef['renderEditCell'];
