df19b0d3addc453af9854bb98de1a7fe65654d1d
Christian Girling <girling.christian@gmail.com>
2022-03-21 17:45:25 -0700

    style - nav-bar styling

--- client/src/Components/NavBarMUI.jsx
+++ client/src/Components/NavBarMUI.jsx
@@ -1,7 +1,13 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
+import { UserContext } from '../Providers/UserProvider';
 import { styled, alpha } from '@mui/material/styles';
+
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
@@ -16,24 +22,29 @@ import MailIcon from '@mui/icons-material/Mail';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import MoreIcon from '@mui/icons-material/MoreVert';
 
-import { Link } from 'react-router-dom';
-import { UserContext } from '../Providers/UserProvider';
-
-// const Search = styled('div')(({ theme }) => ({
-//   position: 'relative',
-//   borderRadius: theme.shape.borderRadius,
-//   backgroundColor: alpha(theme.palette.common.white, 0.15),
-//   '&:hover': {
-//     backgroundColor: alpha(theme.palette.common.white, 0.25),
-//   },
-//   marginRight: theme.spacing(2),
-//   marginLeft: 0,
-//   width: '100%',
-//   [theme.breakpoints.up('sm')]: {
-//     marginLeft: theme.spacing(3),
-//     width: 'auto',
-//   },
+// const Item = styled(Paper)(({ theme }) => ({
+//   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+//   ...theme.typography.body2,
+//   padding: theme.spacing(1),
+//   textAlign: 'center',
+//   color: theme.palette.text.secondary,
 // }));
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(3),
+    width: 'auto',
+  },
+}));
 
 // const SearchIconWrapper = styled('div')(({ theme }) => ({
 //   padding: theme.spacing(0, 2),
@@ -45,19 +56,20 @@ import { UserContext } from '../Providers/UserProvider';
 //   justifyContent: 'center',
 // }));
 
-// const StyledInputBase = styled(InputBase)(({ theme }) => ({
-//   color: 'inherit',
-//   '& .MuiInputBase-input': {
-//     padding: theme.spacing(1, 1, 1, 0),
-//     // vertical padding + font size from searchIcon
-//     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-//     transition: theme.transitions.create('width'),
-//     width: '100%',
-//     [theme.breakpoints.up('md')]: {
-//       width: '20ch',
-//     },
-//   },
-// }));
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    // paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    paddingLeft: `1em`, // CHANGED FROM ABOVE LINE
+    transition: theme.transitions.create('width'),
+    // width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
 
 export default function PrimarySearchAppBar() {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -89,6 +101,11 @@ export default function PrimarySearchAppBar() {
     setMobileMoreAnchorEl(event.currentTarget);
   };
 
+  const handleLogout = () => {
+    handleMenuClose();
+    logout();
+  }
+
   const menuId = 'primary-search-account-menu';
   const renderMenu = (
     <Menu
@@ -108,6 +125,7 @@ export default function PrimarySearchAppBar() {
     >
       <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
       <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+      <MenuItem onClick={handleLogout}>Log out</MenuItem>
     </Menu>
   );
 
@@ -128,16 +146,15 @@ export default function PrimarySearchAppBar() {
       open={isMobileMenuOpen}
       onClose={handleMobileMenuClose}
     >
-      <MenuItem>
+      {/* <MenuItem>
         <IconButton size="large" aria-label="show 4 new mails" color="inherit">
           <Badge badgeContent={4} color="error">
             <MailIcon />
           </Badge>
         </IconButton>
         <p>Messages</p>
-      </MenuItem>
+      </MenuItem> */}
       <MenuItem>
-      
         <IconButton
           size="large"
           aria-label="show 17 new notifications"
@@ -174,12 +191,9 @@ export default function PrimarySearchAppBar() {
             color="inherit"
             aria-label="open drawer"
             sx={{ mr: 2 }}
-            onClick={() => console.log('HI')}
+            onClick={handleMobileMenuOpen}
           >
-            <MenuIcon>
-              
-            </MenuIcon>
-        
+         <img src="https://i.imgur.com/J80OlIF.png" alt='🥖' style ={{maxWidth: "110px"}}/>
           </IconButton>
           <Typography
             variant="h6"
@@ -187,17 +201,41 @@ export default function PrimarySearchAppBar() {
             component="div"
             sx={{ display: { xs: 'none', sm: 'block' } }}
           >
-            Neighbor Cooks
           </Typography>
           
-          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ flexGrow: 1 }}>
+            <Toolbar>
+              <Stack direction='row' spacing={2}>
+                  <Link component={Typography} to="/" style={{ textDecoration: 'none', fontSize: 'larger', color: '#FFF' }}>
+                    <MenuItem>
+                      Browse
+                    </MenuItem>
+                  </Link>
+                {user.userId && <MenuItem>
+                  <Link component={Typography} to="/orders/cart">
+                    My Cart
+                  </Link>
+                </MenuItem>}
+                {user.userId && <MenuItem>
+                  <Link component={Typography} to="/orders/previous">
+                  Previous Orders
+                  </Link>
+                </MenuItem>}
+                {user.userId && user.isVendor && <MenuItem>
+                <Link component={Typography} to="/orders/vendor">
+                  Current Orders
+                  </Link>
+                </MenuItem>}
+                {user.userId && user.isVendor && <MenuItem>
+                  <Link component={Typography} to="/dishes/new">
+                    New Dishes
+                  </Link>
+                </MenuItem>}
+              </Stack>
+            </Toolbar>
+          </Box>
           <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-            <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-              <Badge badgeContent={4} color="error">
-                <MailIcon />
-              </Badge>
-            </IconButton>
-            <IconButton
+            {/* <IconButton
               size="large"
               aria-label="show 17 new notifications"
               color="inherit"
@@ -205,7 +243,26 @@ export default function PrimarySearchAppBar() {
               <Badge badgeContent={17} color="error">
                 <NotificationsIcon />
               </Badge>
-            </IconButton>
+            </IconButton> */}
+            {!userId && 
+            <Toolbar>
+              <Search>
+                <StyledInputBase
+                  placeholder="Log In"
+                  inputProps={{ 'aria-label': 'search' }}
+                  value={input}
+                  onChange={e => setInput(e.target.value)}
+                />
+              </Search>
+              <Button
+                size="large"
+                color="inherit"
+                onClick={e => login(e, input)}
+              >
+                Log In
+              </Button>
+            </Toolbar>
+            }
             <IconButton
               size="large"
               edge="end"
@@ -224,7 +281,7 @@ export default function PrimarySearchAppBar() {
               aria-label="show more"
               aria-controls={mobileMenuId}
               aria-haspopup="true"
-              onClick={handleMobileMenuOpen}
+              onClick={console.log("This is the baguette")}
               color="inherit"
             >
               <MoreIcon />
