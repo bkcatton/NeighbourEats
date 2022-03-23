905ec90aac58647db822eef0b8f1e1d0f1fd31eb
Christian Girling <girling.christian@gmail.com>
2022-03-23 16:02:31 -0700

    style - previous orders

--- client/src/Components/PreviousOrders/index.jsx
+++ client/src/Components/PreviousOrders/index.jsx
@@ -1,13 +1,19 @@
-import React, { useState, useEffect, useContext } from 'react';
-import axiosConfig from '../../axiosConfig';
-import { UserContext } from '../../Providers/UserProvider';
-import LeaveReview from './LeaveReview';
+import React, { useState, useEffect, useContext, Fragment } from "react";
+import axiosConfig from "../../axiosConfig";
+import { UserContext } from "../../Providers/UserProvider";
+import LeaveReview from "./LeaveReview";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import CardActions from "@mui/material/CardActions";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 const PreviousOrders = () => {
   const [orders, setOrders] = useState([]);
   const [showReviewInput, setShowReviewInput] = useState(false);
   const [dishId, setDishId] = useState(null);
-  const [reviewBody, setReviewBody] = useState('');
+  const [reviewBody, setReviewBody] = useState("");
   const { user } = useContext(UserContext);
   const { userId } = user;
 
@@ -28,43 +34,65 @@ const PreviousOrders = () => {
     }
   }, [userId]);
 
-  const toggleShow = id => {
+  const toggleShow = (id) => {
     setDishId(id);
     setShowReviewInput(true);
   };
 
   const handleSubmit = async () => {
     try {
-      await axiosConfig.post('/orders/reviews', {
+      await axiosConfig.post("/orders/reviews", {
         userId,
         dishId,
-        reviewBody
-      })
-
+        reviewBody,
+      });
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
-  const ordersList = orders.map(item => {
+  const ordersList = orders.map((item, i) => {
     const { dish_id, title, paid_price_cents, quantity, image_link } = item;
+
     return (
-      <div key={dish_id}>
-        {title}
-        {quantity}
-        {paid_price_cents}
-        <img src={image_link} alt={title} style={{ maxWidth: '100px' }} />
-        <button onClick={() => toggleShow(dish_id)}>Leave Review</button>
-      </div>
+      <Card key={i} variant="outlined">
+        <CardContent>
+          <Typography sx={{ fontSize: 20 }} gutterBottom>
+            {item.title}
+          </Typography>
+          <CardMedia
+            component="img"
+            src={item.image_link}
+            alt={item.title}
+            style={{ width: "200px" }}
+          />
+          <Typography variant="body2">{`${item.paid_price_cents}`}</Typography>
+        </CardContent>
+        <CardActions>
+          <LeaveReview
+            reviewBody={reviewBody}
+            setReviewBody={setReviewBody}
+            handleSubmit={handleSubmit}
+          />
+        </CardActions>
+      </Card>
     );
   });
 
   return (
-    <div>
-      PreviousOrders
-      <ul>{ordersList}</ul>
-      {showReviewInput && <LeaveReview reviewBody={reviewBody} setReviewBody={setReviewBody} handleSubmit={handleSubmit} />}
-    </div>
+    <Fragment>
+      <Box sx={{ minWidth: 275 }}>
+        <Typography
+          variant="h4"
+          gutterBottom
+          component="div"
+          textAlign="center"
+        >
+          Previous Orders
+        </Typography>
+        {ordersList.length ? ordersList : <Typography>No Orders</Typography>}
+      </Box>
+    </Fragment>
   );
 };
 
