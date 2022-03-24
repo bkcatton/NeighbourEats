1f2bf2f6908adcd232d60682a3194e3478f7712b
bkcatton <ben.k.catton@gmail.com>
2022-03-23 18:03:14 -0700

    style - orders modal adding to cart transition

--- client/src/Components/Browse/DishModal/AddToOrder.jsx
+++ client/src/Components/Browse/DishModal/AddToOrder.jsx
@@ -1,27 +1,35 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, Fragment } from 'react'
 import { Link } from 'react-router-dom';
 import axiosConfig from '../../../axiosConfig';
 import { UserContext } from '../../../Providers/UserProvider'
 import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
+import LoadingButton from '@mui/lab/LoadingButton';
+import { Typography } from '@mui/material';
 
 const AddToOrder = props => {
   const [quantity, setQuantity] = useState(1);
   const [checkout, setCheckout] = useState(false);
+  const [loading, setLoading] = useState(false);
   const { user } = useContext(UserContext);
   const { userId } = user;
 
- // create an order, use newly created order_id to add order with details to cart
+  // create an order, use newly created order_id to add order with details to cart
   const onSubmit = () => {
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+      setCheckout(true)
+    }, 1500);
+
     axiosConfig.post('/orders', { userId })
-      .then(data => {  
+      .then(data => {
         const { id: order_id } = data.data;
         const { id: dish_id } = props.dishDetails;
         const paid_price_cents = Number(props.dishDetails.price_cents);
-
         // create new order item using newly created order id
         axiosConfig.post(`/orders/order_item`, { order_id, dish_id, quantity, paid_price_cents })
-          .then(setCheckout(true))
           .catch(error => console.log(error))
       })
       .catch(error => console.log(error))
@@ -29,19 +37,28 @@ const AddToOrder = props => {
 
   return (
     <div>
-      <ButtonGroup>      
-      { (quantity > 1) && <Button onClick={() => setQuantity(prev => prev - 1)}>-</Button>}
-      <Button  style={{color:"black"}} disabled><strong>{quantity}</strong></Button>
-      <Button onClick={() => setQuantity(prev => prev + 1)}>+</Button>
-      </ButtonGroup>
-      {!checkout ? <Button onClick={onSubmit} variant='outlined'>Add to Order</Button>
+      <ButtonGroup>
+        {!checkout ? 
+        <Fragment>
+
+        <Button disabled={quantity < 2} onClick={() => setQuantity(prev => prev - 1)}>-</Button>
+        <Button style={{ color: "black" }} disabled><strong>{quantity}</strong></Button>
+        <Button onClick={() => setQuantity(prev => prev + 1)}>+</Button>
+        </Fragment>
       :
-      <Button color="success">Added!</Button>}
-      {checkout && <Button component={Link} to="/orders">Continue to Checkout</Button>}
-      {checkout && <Button component={Link} to="/">Continue Shopping</Button>}
+        <Button variant="text" sx={{ color: "black" }}>Quantity: {quantity}</Button>
+      }
+      </ButtonGroup>
+      {!checkout && !loading && <Button fullWidth onClick={onSubmit} variant='outlined'>Add to Order</Button>}
+      {loading &&
+        <LoadingButton fullWidth loading variant="outlined">
+          Submit
+        </LoadingButton>}
+      {checkout && <Button fullWidth color="success">Added!</Button>}
+      <Button fullWidth component={Link} to={`/orders/cart`}>View Cart</Button>
     </div>
   )
-  
+
 }
 
 export default AddToOrder
