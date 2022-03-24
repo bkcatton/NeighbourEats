0e734101920433e36fcafce1be89e5c5280889b5
Christian Girling <girling.christian@gmail.com>
2022-03-24 09:55:31 -0700

    style - search bar

--- client/src/Components/Browse/SearchInput.jsx
+++ client/src/Components/Browse/SearchInput.jsx
@@ -2,61 +2,27 @@ import React from 'react'
 import SearchIcon from '@mui/icons-material/Search';
 import InputBase from '@mui/material/InputBase';
 import { styled, alpha } from '@mui/material/styles';
-
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(3),
-    width: 'auto',
-  },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
-  },
-}));
+import {  TextField, IconButton } from '@material-ui/core';
+import { SearchOutlined } from '@material-ui/icons';
 
 const SearchInput = (props) => {
 
   return (
-    <Search>
-      <SearchIconWrapper>
-        <SearchIcon />
-      </SearchIconWrapper>
-      <StyledInputBase
-        placeholder="Search…"
-        inputProps={{ 'aria-label': 'search' }}
-        value={props.searchValue}
-        onChange={(e) => props.setSearchValue(e.target.value)}
-      />
-    </Search>
+    <TextField
+      fullWidth
+      id="standard-bare"
+      variant="outlined"
+      placeholder="Find a dish!"
+      value={props.searchValue}
+      onChange={(e) => props.setSearchValue(e.target.value)}
+      InputProps={{
+        endAdornment: (
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+        ),
+      }}
+    />
   )
 }
 
